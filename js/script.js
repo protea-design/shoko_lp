614a979c930944920a7afa2ba1cbc4ade02a7ab8
yuna-sasaki <nyu_ssk_stz@au.com>
2024-02-26 20:59:49 +0900

    コンテンツ追加　img追加

--- js/script.js
+++ js/script.js
@@ -1,13 +1,33 @@
-const swiper = new Swiper('.swiper', {
-    loop: true,
-    autoplay: {
-        delay: 0,
-    },
-    speed: 3000,
-    slidesPerView: 4,
-    breakpoints: {
-        767: {
-          slidesPerView: 7,
-        }
-      }
+// FV Swiper
+const swiper = new Swiper('.fv_swiper', {
+  loop: true,
+  autoplay: {
+    delay: 0,
+  },
+  speed: 3000,
+  slidesPerView: 4,
+  breakpoints: {
+    767: {
+      slidesPerView: 7,
+    }
+  }
+});
+
+// 導入事例 Swiper
+const case_swiper = new Swiper('.case_swiper', {
+  loop: true,
+  slidesPerView: 1,
+});
+
+
+$(window).scroll(function () {
+  $('.slidein').each(function () {
+    var elemPos = $(this).offset().top,
+      scroll = $(window).scrollTop(),
+      windowHeight = $(window).height();
+
+    if (scroll > elemPos - windowHeight + 150) {
+      $(this).addClass('scrollin');
+    }
   });
+});
